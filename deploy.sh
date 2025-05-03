53dc7500ff67dcf79932f88a13d2b9c39221dbb7
JeremiahVaughan <jeremiah.t.vaughan@gmail.com>
2025-05-03 10:46:35 -0500

    working on maintenance page

--- deploy.sh
+++ deploy.sh
@@ -1,22 +1,24 @@
+#!/bin/bash
 set -e
 
+export APP=maint
+FILES=("./deploy/")
+
 DEST=$1
-APP=maint
-
 DEPLOY_PATH="${HOME}/deploy/${APP}"
-
 if [ "$DEST" = "remote" ]; then            
-    GOOS=linux GOARCH=arm64 go build -o ./deploy/app
+    GOOS=linux GOARCH=arm64 go build -o "./deploy/${APP}"
     REMOTE_HOST="deploy.target"                                                     
     REMOTE_PATH="${REMOTE_HOST}:${DEPLOY_PATH}"                     
-    rsync -avz --delete -e ssh ./deploy/ "$REMOTE_PATH"
-    ssh "${REMOTE_HOST}"  "${DEPLOY_PATH}/remote-deploy.sh"
+    rsync -avz --delete -e ssh "${FILES[@]}" "$REMOTE_PATH"          
+    ssh "${REMOTE_HOST}" "APP=${APP} ${DEPLOY_PATH}/remote-deploy.sh"
 else                                                                  
-    GOOS=linux GOARCH=amd64 go build -o ./deploy/app
+    GOOS=linux GOARCH=amd64 go build -o "./deploy/${APP}"
     LOCAL_PATH="${DEPLOY_PATH}"                               
-    rsync -avz --delete ./deploy/ "$LOCAL_PATH"
+    rsync -avz --delete "${FILES[@]}" "$LOCAL_PATH"
     "${DEPLOY_PATH}/remote-deploy.sh"
 fi                                                                    
+
 
 
 
--- deploy/remote-deploy.sh
+++ deploy/remote-deploy.sh
@@ -1,7 +1,6 @@
-APP=maint
-
+#!/bin/bash
 set -e
-sudo cp "$HOME/deploy/${APP}/${APP}.service" "/etc/systemd/system/${APP}.service"
+sudo cp "${HOME}/deploy/${APP}/${APP}.service" "/etc/systemd/system/${APP}.service"
 sudo systemctl enable "${APP}.service"
 sudo systemctl start "${APP}.service"
 sudo systemctl restart "${APP}.service"
